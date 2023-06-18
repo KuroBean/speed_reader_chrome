f010f79c8cceacc77203fcc192966c6a4be9e0e4
Kensuke <kenshimojo@icloud.com>
2023-06-18 01:01:29 -0700

    time adjsutment by word length working, so is last syllable bolding

--- content.js
+++ content.js
@@ -3,33 +3,36 @@ var words = [];
 var index = 0;
 var cumulativeText = "";
 var timeoutId;
-var pauseKeys = [".", ",", ";", ":"];
+var pauseKeys = [".", ",", ";", ":", ")", "]"];
 var pauseTimeForKeys = 300;
 var regularPauseTime = 150;
 var timeInterval;
+var timeIncPerChar = 20; // Adjust this value to control the time increment per character
+var timeUpperCap=500;
+var timeLowerCap=75;
 
 //PUT DOCUMENT EVENT LISTENERS HERE, NOT AT BOTTOM
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   // Your code to set the inputText value here
-// Retrieve stored text from extension's storage
-chrome.storage.local.get(['highlightedText'], function(result) {
-  console.log("getting stored text");
-  var inputText = document.getElementById('inputText');
-  inputText.value = result.highlightedText || '';
+  // Retrieve stored text from extension's storage
+  chrome.storage.local.get(['highlightedText'], function (result) {
+    console.log("getting stored text");
+    var inputText = document.getElementById('inputText');
+    inputText.value = result.highlightedText || '';
+  });
 });
-});
-document.addEventListener("mouseup", handleSelection); 
+document.addEventListener("mouseup", handleSelection);
 
 //document.addEventListener("click",function(){console.log("beans")});
 
-function handleSelection() { 
+function handleSelection() {
   console.log("highlighting detected")
-  var selectedText = window.getSelection().toString(); 
+  var selectedText = window.getSelection().toString();
   if (selectedText.length > 0) {
-    console.log("Selected text:", selectedText); 
-    chrome.runtime.sendMessage({ text: selectedText }); 
+    console.log("Selected text:", selectedText);
+    chrome.runtime.sendMessage({ text: selectedText });
     console.log("msg sent");
-  } 
+  }
 }
 
 function displayWordsWithBoldSyllables() {
@@ -48,6 +51,7 @@ function displayWordsWithBoldSyllables() {
   displayNextWord();
 }
 
+
 function displayNextWord() {
   if (index >= words.length) {
     return;
@@ -55,20 +59,24 @@ function displayNextWord() {
 
   var word = words[index];
   var firstSyllable = getFirstSyllable(word);
-  var boldSyllable = "<b>" + firstSyllable + "</b>";
-  var remainingLetters = word.substr(firstSyllable.length);
-  var formattedWord = boldSyllable + remainingLetters;
+  var lastSyllable = getLastSyllable(word);
+  var formattedWord = "";
+
+  if (countVowels(word) === 1) {
+    formattedWord = "<b>" + firstSyllable + "</b>" + word.substr(firstSyllable.length);
+  } else {
+    formattedWord = "<b>" + firstSyllable + "</b>" + word.substr(firstSyllable.length, word.length - firstSyllable.length - lastSyllable.length) + "<b>" + lastSyllable + "</b>";
+  }
 
   var outputDiv = document.getElementById("output");
   var cumulativeOutputDiv = document.getElementById("cumulativeOutput");
 
-   // Get the user-inputted font size for the flashing text
+  // Get the user-inputted font size for the flashing text
   var outputFontSize = document.getElementById("outputFontSize").value;
 
   // Apply the font size to the output div
   outputDiv.style.fontSize = outputFontSize + "px";
-  
-  cumulativeOutputDiv.style.fontSize=18+"px";
+  cumulativeOutputDiv.style.fontSize = 18 + "px";
 
   outputDiv.innerHTML = formattedWord;
   cumulativeText += formattedWord + " ";
@@ -76,14 +84,28 @@ function displayNextWord() {
 
   index++;
 
+  var wordLength = word.length;
+  var timeAdjustment = (wordLength - 5) * timeIncPerChar;
+
   if (pauseKeys.includes(word.charAt(word.length - 1))) {
     timeInterval = pauseTimeForKeys;
+    //length adjustment doesn't kick in for already longer pausing words with pausekeys
   } else {
-    timeInterval = regularPauseTime;
+    timeInterval = regularPauseTime + timeAdjustment;
+  }
+
+  // Cap the timeInterval between 50ms and 400ms
+  if (timeInterval > timeUpperCap) {
+    timeInterval = timeUpperCap;
+  } else if (timeInterval < timeLowerCap) {
+    timeInterval = timeLowerCap;
   }
 
   timeoutId = setTimeout(displayNextWord, timeInterval);
 }
+
+// Rest of the code remains the same
+
 
 function getFirstSyllable(word) {
   var vowels = ["a", "e", "i", "o", "u", "y"];
@@ -97,6 +119,38 @@ function getFirstSyllable(word) {
   }
 
   return firstSyllable;
+}
+
+function getLastSyllable(word) {
+  var vowels = ["a", "e", "i", "o", "u", "y"];
+  var lastSyllable = "";
+
+  var lastIndex = -1;
+
+  for (var i = 0; i < word.length; i++) {
+    if (vowels.includes(word[i])) {
+      lastIndex = i;
+    }
+  }
+
+  if (lastIndex > -1) {
+    lastSyllable = word.substr(lastIndex);
+  }
+
+  return lastSyllable;
+}
+
+function countVowels(word) {
+  var vowels = ["a", "e", "i", "o", "u", "y"];
+  var count = 0;
+
+  for (var i = 0; i < word.length; i++) {
+    if (vowels.includes(word[i])) {
+      count++;
+    }
+  }
+
+  return count;
 }
 
 function pauseAnimation() {
