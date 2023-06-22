7ef403354793c6104accdb3bdbed1783fc39e589
Kensuke <kenshimojo@icloud.com>
2023-06-22 01:47:24 -0700

    popup location adjustment

--- custombutton.js
+++ custombutton.js
@@ -1,5 +1,4 @@
 let button = null; // Variable to hold the reference to the button
-//PUT DOCUMENT EVENT LISTENERS HERE, NOT AT BOTTOM
 var prevHighlightedText="default";
 
 
@@ -27,7 +26,7 @@ function handleHighlight() {
                 button.addEventListener("click", handlePopup);
                 console.log("button eventlistener added");
                 document.body.appendChild(button);
-            }, 200);
+            }, 100);
 
         }
     }
@@ -53,38 +52,39 @@ function positionButton(button) {
         button.style.top = rect.top + window.pageYOffset + "px";
     }
 }
-// Create a function to handle the popup
+// Function to handle the popup window
 function handlePopup() {
-    console.log('handlePopup() has been run.');
-
-
-    // Open a new window
-    const popupWindow = window.open('', '', 'width=500,height=500');
-
-    // Get the position of the selected text
-    const selection = window.getSelection();
-    const range = selection.getRangeAt(0);
-    const rect = range.getBoundingClientRect();
-    const scrollLeft = window.scrollX || document.documentElement.scrollLeft;
-    const scrollTop = window.scrollY || document.documentElement.scrollTop;
-
-    // Calculate the position for the popup window
-    const popupLeft = rect.left + scrollLeft;
-    const popupTop = rect.top + scrollTop - 500;
-
+    console.log("handlePopup() has been run.");
+  
+    // Get the position of the button
+    const buttonLeft = button.offsetLeft;
+    const buttonTop = button.offsetTop;
+  
+    // Define the width and height of the popup window
+    const popupWidth = 750;
+    const popupHeight = 500;
+  
+    // Open the new window
+    const popupWindow = window.open("", "", `width=${popupWidth},height=${popupHeight}`);
+  
     // Set the position of the popup window
+    const popupLeft = buttonLeft - window.innerWidth + popupWidth;
+    const popupTop = buttonTop - popupHeight;
     popupWindow.moveTo(popupLeft, popupTop);
-    popupWindow.resizeTo(500, 500);
-
+    popupWindow.resizeTo(popupWidth, popupHeight);
+  
     // Load the content of index.html in the popup window
-    popupWindow.document.write('<html><head><title>Popup Window</title></head><body></body></html>');
-    const popupDocument = popupWindow.document.querySelector('body');
-    popupDocument.innerHTML = `<object type="text/html" data="${chrome.extension.getURL('index.html')}" style="width: 100%; height: 100%;"></object>`;
-
-    // Add event listener to close the popup when interacting with the webpage outside the popup
-    window.addEventListener('click', function (event) {
-        if (event.target !== popupWindow && event.target !== button) {
-            popupWindow.close();
-        }
+    popupWindow.document.write(
+      `<html><head><title>Popup Window</title></head><body><object type="text/html" data="${chrome.extension.getURL(
+        "index.html"
+      )}" style="width: 100%; height: 100%;"></object></body></html>`
+    );
+  
+    // Add an event listener to close the popup when interacting with the webpage outside the popup
+    window.addEventListener("click", function (event) {
+      if (event.target !== popupWindow && event.target !== button) {
+        popupWindow.close();
+      }
     });
-}
+  }
+  
